baff4c7ea76115192a99372a3881a78b94b1bc00
Noam Ureta-Vidal <102073970+noamuv@users.noreply.github.com>
2025-10-29 22:46:43 +0000

    Update Projects.jsx

--- src/react/pages/Projects.jsx
+++ src/react/pages/Projects.jsx
@@ -3,9 +3,9 @@ export default function Projects() {
         <div className="container">
             <div className="projects__wrapper">
                 <ul>
-                    <File href="https://noamuv.github.io/sushi_website_tutorial/" name="Sushi Website" date="Nov 2024" code="HTML/CSS"/>
                     <File href="https://github.com/noamuv/AI-News-Summariser" name="AI News Summariser" date="May 2025" code="Java"/>
                     <File href="https://github.com/noamuv/Second-Hand-Car-Store-App" name="Second Hand Car Store App" date="Dec 2024" code="C Programming"/>
+                    <File href="https://noamuv.github.io/sushi_website_tutorial/" name="Sushi Website" date="Nov 2024" code="HTML/CSS"/>
                 </ul>
             </div>
         </div>
